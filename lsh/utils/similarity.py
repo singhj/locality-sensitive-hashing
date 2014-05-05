171d508bb1cae8fadd0fcb6e51914539aef1948c
teresa brooks <teresa.brooks@gmail.com>
2014-05-05 00:36:47 -0400

    hide implementation of function by more than convention

--- lsh/utils/similarity.py
+++ lsh/utils/similarity.py
@@ -3,7 +3,7 @@
 def jaccard_similarity(set1, set2):
     return _calculate_jaccard_similairty(set1, set2)
 
-def _calculate_jaccard_similairty(set1, set2):
+def __calculate_jaccard_similairty(set1, set2):
     x = len(set1.intersection(set2))
     y = len(set1.union(set2))
 
