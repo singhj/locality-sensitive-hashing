7c70e7bd41088f09e415e893932b8313de36557f
teresa brooks <teresa.brooks@gmail.com>
2014-06-10 23:24:21 -0400

    * implemented first pass, untested basic implementation of minhash using XORing random numbers against hashed values of shingles to implement the need  n number of random hashing algorithms * implemented a thread safe random number generator that returns n number of k bit randomly generated long ints * Note all this code is untested...just wanted to get this pushed for review and so I have the idea down...

--- lsh/minhash/minhash.py
+++ lsh/minhash/minhash.py
@@ -1,3 +1,43 @@
-"""
-all things minhash goes here
-"""
+from operator import xor
+from lsh.utils.random_number_generator import RNG
+import heapq
+
+DEFAULT_NUM_RANDOM_NUMS = 200 #TODO should create global config to set this
+DEFAULT_BITS = 32 #TODO should create global config to set this
+
+RANDOM_NUMBERS = RNG.instance(DEFAULT_NUM_RANDOM_NUMS, DEFAULT_BITS)
+
+def run(shingles_generator):
+
+    #basic minhash implementation algorithm steps...
+    min_hash_values = []
+
+    if shingles_generator:
+        for shingle in shingles_generator():
+            # reset min-heap as each shingle should get it's own min-heap
+            # to calculate the minimum hash value
+            min_heap = []
+
+            for count in range(0, DEFAULT_NUM_RANDOM_NUMS):
+
+                #step 1: calculate hash values for current shingle
+                if count == 0:
+                    shingle_hash = calc_hash(shingle)
+                else:
+                    num = RANDOM_NUMBERS[count]
+                    shingle_hash = do_xor(calc_hash(shingle), num)
+
+                #step 2: add to min-heap
+                heapq.heappush(min_heap, shingle_hash)
+
+            #step 3: select minimum value from min-heap and add to return list
+            min_value = heapq.heappop(min_heap) #note, default behavior for pop gives min value
+            min_hash_values.append(min_value)
+
+    return min_hash_values
+
+def calc_hash(value):
+    return hash(value)
+
+def do_xor(a, b):
+    return xor(a,b)
---
+++ lsh/utils/random_number_generator.py
@@ -0,0 +1,38 @@
+import random
+
+class RNG(object):
+    """
+        Implements a thread safe, random number generator. It generates a list of n random numbers. By default
+        all numbers are 32bit randomly generated numbers. Users can specify the number of random numbers to be
+        generated.
+
+        This class is designed such that all threads will have access the same random numbers. This implementation
+        is specific to the needs of our minhash algorithm. In order to implement n number hash functions we use n
+        number random generated numbers and XOR those numbers against hashed values. All documents we process need to
+        use the same random numbers. In order for our LSH implementation to be scalable each thread / instance should
+        have it's own set of generated numbers and this class enables us to do this.
+
+    """
+
+    __lock = threading.Lock()
+    __instance = None
+    __random_numbers = None
+
+    @classmethod
+    def instance(cls, n=1, bits=32):
+        if not cls.__instance and not cls.__random_numbers:
+            with cls.__lock:
+                if not cls.__instance and not cls.__random_numbers:
+                    cls.__instance = RNG()
+                    cls.__random_numbers = cls.__generate_random_numbers(n, bits)
+        return cls.__random_numbers
+
+    @classmethod
+    def __generate_random_numbers(cls, n=1, bits=32):
+        """
+            Generates n random numbers.
+            :param n: number of random numbers to generate
+            :param bits: number of bits each random number should contain.
+            :return: list of long integer random numbers.
+        """
+        return [random.getrandbits(bits) for _ in range(0, n)]
