fe4c0733c5e359010e45262c8e3d4b0b9d31fe8a
singhj <j.singh@earlystageit.com>
2014-09-30 11:46:44 -0400

    Do the computation in a separate task

--- blobs.py
+++ blobs.py
@@ -1,4 +1,4 @@
-import os
+import re
 import urllib
 import webapp2
 import logging
@@ -6,6 +6,7 @@ import zipfile
 
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
+from google.appengine.api import taskqueue
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
@@ -13,7 +14,14 @@ class MainHandler(webapp2.RequestHandler):
         self.response.out.write('<html><body>')
         self.response.out.write('<ol>')
         for blob_info in blobstore.BlobInfo.all():
-            self.response.out.write('<li><a href="/serve_blob/%s">%s</a> %s</li>' % (blob_info.key(), blob_info.filename, [('%s: %s' % (p, str(getattr(blob_info, p)))) for p in blob_info._all_properties if p not in ('md5_hash','filename',)]))
+            blob_key = blob_info.key()
+            blob_reader = blobstore.BlobReader(blob_key)
+            zip_reader = zipfile.ZipFile(blob_reader)
+            url_file_reader = zip_reader.open('url.out')
+            line_count = 0
+            while url_file_reader.readline():
+                line_count += 1
+            self.response.out.write('<li><a href="/serve_blob/%s">%s</a> %s %d urls</li>' % (blob_info.key(), blob_info.filename, [('%s: %s' % (p, str(getattr(blob_info, p)))) for p in blob_info._all_properties if p not in ('md5_hash','filename',)], line_count))
         self.response.out.write('</ol>')
         self.response.out.write('<form action="%s" method="POST" enctype="multipart/form-data">' % upload_url)
         self.response.out.write("""Upload File: <input type="file" name="file"><br> <input type="submit"
@@ -31,15 +39,42 @@ class ServeHandler(blobstore_handlers.BlobstoreDownloadHandler):
         blob_reader = blobstore.BlobReader(blob_key)
         zip_reader = zipfile.ZipFile(blob_reader)
         logging.info('contents: %s', zip_reader.namelist())
-        url_file_reader = zip_reader.open('url.out')
-        for line in xrange(3):
-            logging.info('    line %d: %s', line, url_file_reader.readline()) 
+        url_file_pattern = re.compile('^."id":"([^"]*)","url":"([^"]*)".*')
+        lno = 0
+        urls = {}
+        with zip_reader.open('url.out') as url_file_reader:
+            for line in url_file_reader:
+                lno += 1
+                found_pattern  = url_file_pattern.search(line)
+                urls[found_pattern.group(1)] = found_pattern.group(2)
+                if lno < 3:
+                    logging.info('    line %d: %s: %s', lno, found_pattern.group(1), found_pattern.group(2))
+        logging.info('url.out: %d ids', len(urls.keys()))
+
+        taskqueue.add(url='/text_worker', params={'key': blob_key})
+                
         self.redirect('/blobs')
-#         resource = str(urllib.unquote(resource))
-#         blob_info = blobstore.BlobInfo.get(resource)
-#         logging.info('blob_info: %s', [('%s: %s' % (p, str(getattr(blob_info, p)))) for p in blob_info._all_properties])
-#         self.send_blob(blob_info, save_as = True)
+
+class TextWorker(webapp2.RequestHandler):
+    def post(self): # should run at most 1/s due to entity group limit
+        blob_key = self.request.get('key')
+        blob_reader = blobstore.BlobReader(blob_key)
+        zip_reader = zipfile.ZipFile(blob_reader)
+        text_file_pattern = re.compile('^."id":"([^"]*):html","text":"(.*".*).', flags=re.DOTALL)
+        lno = 0
+        texts = {}
+        with zip_reader.open('text.out') as text_file_reader:
+            for line in text_file_reader:
+                found_pattern = text_file_pattern.search(line)
+                if found_pattern:
+#                     texts[found_pattern.group(1)] = found_pattern.group(2)
+                    lno += 1
+                    if lno < 3:
+                        logging.info('    line %d: %s: %d characters', lno, found_pattern.group(1), len(found_pattern.group(2)))
+        logging.info('text.out: %d ids', lno)
 
 urls = [('/blobs', MainHandler),
         ('/upload_blob', UploadHandler),
-        ('/serve_blob/([^/]+)?', ServeHandler)]
+        ('/serve_blob/([^/]+)?', ServeHandler),
+        ('/text_worker', TextWorker),
+        ]
