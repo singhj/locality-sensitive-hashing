9c069ab1e520de454ddbc1f44bdb4154455d942d
J Singh <singhj@users.noreply.github.com>
2015-02-02 20:35:56 -0500

    Set default to 'datastore'

--- settings.py
+++ settings.py
@@ -8,7 +8,7 @@ max_mask = 2**max_bits - 1
 
 DATABASES = {
     'default': {
-        'ENGINE': '',       # Add 'cassandra' or 'gae' or leave blank
+        'ENGINE': 'datastore',       # Add 'cassandra' or 'datastore' or leave blank
         'KEYSPACE': 'datathinks',    # Keyspace if using Cassandra
 
         # the stuff below this line is not used at the moment but may come in handy if we ever user other dbs
@@ -19,4 +19,3 @@ DATABASES = {
         'PORT': '',     # Set to empty string for default.
     },
 }
-    
